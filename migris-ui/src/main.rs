4286afb84cd552aca5ad8e01d8db464e87feae57
Justin Doornbos <doornbosjustin@gmail.com>
2026-03-15 13:42:46 -0400

    feat(ui): change default font size & make connection list scrollable

--- migris-ui/src/main.rs
+++ migris-ui/src/main.rs
@@ -2,11 +2,19 @@ mod app;
 mod message;
 mod widgets;
 
+use iced::{Pixels, Settings};
+
 use crate::app::Application;
 
 fn main() -> iced::Result {
+    let settings = Settings {
+        fonts: vec![include_bytes!("../assets/fonts/lucide.ttf").into()],
+        default_text_size: Pixels(12.0),
+        ..Default::default()
+    };
+
     iced::application(Application::new, Application::update, Application::view)
         .title("migris")
-        .font(include_bytes!("../assets/fonts/lucide.ttf"))
+        .settings(settings)
         .run()
 }
