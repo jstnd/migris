e8433e5010aa6cc03cd36d675bd72e60be2116cc
Justin Doornbos <doornbosjustin@gmail.com>
2026-05-03 11:14:46 -0400

    refactor(connections): reorganize manager code

--- migris-gpui/src/connections.rs
+++ migris-gpui/src/connections.rs
@@ -15,6 +15,249 @@ use uuid::Uuid;
 
 use crate::shared;
 
+pub struct ConnectionManager {
+    /// The config containing the connections and folders.
+    config: ConnectionConfig,
+
+    /// Tracks the locations of connections within the full list by [`ConnectionId`].
+    connection_map: HashMap<ConnectionId, usize>,
+
+    /// Tracks the connections within each folder.
+    connections_by_folder: HashMap<Option<ConnectionFolderId>, Vec<ConnectionId>>,
+
+    /// Tracks the locations of folders within the full list by [`ConnectionFolderId`].
+    folder_map: HashMap<ConnectionFolderId, usize>,
+
+    /// Tracks the folders within each parent folder.
+    folders_by_parent: HashMap<Option<ConnectionFolderId>, Vec<ConnectionFolderId>>,
+}
+
+impl Global for ConnectionManager {}
+
+impl ConnectionManager {
+    /// Loads the config from the config file.
+    pub fn load() -> Self {
+        let config = Self::try_load().unwrap_or_else(|_| ConnectionConfig::default());
+        let mut manager = Self {
+            config,
+            connection_map: HashMap::new(),
+            connections_by_folder: HashMap::new(),
+            folder_map: HashMap::new(),
+            folders_by_parent: HashMap::new(),
+        };
+
+        manager.load_maps();
+        manager
+    }
+
+    /// Saves the config to the config file.
+    pub fn save(&mut self) {
+        // TODO: log errors with saving
+        _ = self.try_save();
+    }
+
+    /// Returns a reference to the global [`ConnectionManager`].
+    pub fn global(cx: &App) -> &Self {
+        cx.global::<Self>()
+    }
+
+    /// Returns a mutable reference to the global [`ConnectionManager`].
+    pub fn global_mut(cx: &mut App) -> &mut Self {
+        cx.global_mut::<Self>()
+    }
+
+    /// Adds a new connection to the config.
+    pub fn add_connection(&mut self, connection: Connection) {
+        self.config.connections.push(connection);
+        self.load_maps();
+        self.save();
+    }
+
+    /// Adds a new folder to the config.
+    pub fn add_folder(&mut self, folder: ConnectionFolder) {
+        self.config.folders.push(folder);
+        self.load_maps();
+        self.save();
+    }
+
+    /// Returns a reference to the connection matching the given [`ConnectionId`].
+    pub fn connection(&self, id: &ConnectionId) -> &Connection {
+        let idx = self.connection_map[id];
+        &self.config.connections[idx]
+    }
+
+    /// Returns a mutable reference to the connection matching the given [`ConnectionId`].
+    pub fn connection_mut(&mut self, id: &ConnectionId) -> &mut Connection {
+        let idx = self.connection_map[id];
+        &mut self.config.connections[idx]
+    }
+
+    /// Returns the connections within the given folder.
+    pub fn connections_for_folder(
+        &self,
+        folder: &Option<ConnectionFolderId>,
+    ) -> Option<&Vec<ConnectionId>> {
+        self.connections_by_folder.get(folder)
+    }
+
+    /// Returns a reference to the folder matching the given [`ConnectionFolderId`].
+    pub fn folder(&self, id: &ConnectionFolderId) -> &ConnectionFolder {
+        let idx = self.folder_map[id];
+        &self.config.folders[idx]
+    }
+
+    /// Returns the folders within the given parent folder.
+    pub fn folders_for_parent(
+        &self,
+        parent: &Option<ConnectionFolderId>,
+    ) -> Option<&Vec<ConnectionFolderId>> {
+        self.folders_by_parent.get(parent)
+    }
+
+    /// Removes the connection with the given [`ConnectionId`] from the config.
+    pub fn remove_connection(&mut self, id: &ConnectionId) {
+        let idx = self.connection_map[id];
+        self.config.connections.swap_remove(idx);
+        self.load_maps();
+        self.save();
+    }
+
+    /// Removes the folder with the given [`ConnectionFolderId`] from the config.
+    ///
+    /// Returns the set of connections that were removed for future processing if needed.
+    pub fn remove_folder(&mut self, id: &ConnectionFolderId) -> HashSet<ConnectionId> {
+        fn remove_inner(
+            manager: &ConnectionManager,
+            id: ConnectionFolderId,
+        ) -> (HashSet<ConnectionId>, HashSet<ConnectionFolderId>) {
+            let mut removed_connections = HashSet::new();
+            let mut removed_folders = HashSet::from([id]);
+
+            if let Some(connections) = manager.connections_for_folder(&Some(id)) {
+                removed_connections.extend(connections);
+            }
+
+            if let Some(folders) = manager.folders_for_parent(&Some(id)) {
+                for id in folders {
+                    let (inner_removed_connections, inner_removed_folders) =
+                        remove_inner(manager, *id);
+                    removed_connections.extend(inner_removed_connections);
+                    removed_folders.extend(inner_removed_folders);
+                }
+            }
+
+            (removed_connections, removed_folders)
+        }
+
+        let (removed_connections, removed_folders) = remove_inner(self, *id);
+        self.config
+            .connections
+            .retain(|connection| !removed_connections.contains(&connection.id));
+        self.config
+            .folders
+            .retain(|folder| !removed_folders.contains(&folder.id));
+
+        self.load_maps();
+        self.save();
+        removed_connections
+    }
+
+    /// Returns a reference to the connection matching the given id string, if one is found.
+    pub fn try_connection(&self, id: &SharedString) -> Option<&Connection> {
+        if let Ok(uuid) = Uuid::parse_str(id)
+            && let Some(idx) = self.connection_map.get(&ConnectionId(uuid))
+            && let Some(connection) = self.config.connections.get(*idx)
+        {
+            Some(connection)
+        } else {
+            None
+        }
+    }
+
+    /// Returns a mutable reference to the connection matching the given id string, if one is found.
+    pub fn try_connection_mut(&mut self, id: &SharedString) -> Option<&mut Connection> {
+        if let Ok(uuid) = Uuid::parse_str(id)
+            && let Some(idx) = self.connection_map.get(&ConnectionId(uuid))
+            && let Some(connection) = self.config.connections.get_mut(*idx)
+        {
+            Some(connection)
+        } else {
+            None
+        }
+    }
+
+    /// Returns a reference to the folder matching the given id string, if one is found.
+    pub fn try_folder(&self, id: &SharedString) -> Option<&ConnectionFolder> {
+        if let Ok(uuid) = Uuid::parse_str(id)
+            && let Some(idx) = self.folder_map.get(&ConnectionFolderId(uuid))
+            && let Some(folder) = self.config.folders.get(*idx)
+        {
+            Some(folder)
+        } else {
+            None
+        }
+    }
+
+    /// Returns a mutable reference to the folder matching the given id string, if one is found.
+    pub fn try_folder_mut(&mut self, id: &SharedString) -> Option<&mut ConnectionFolder> {
+        if let Ok(uuid) = Uuid::parse_str(id)
+            && let Some(idx) = self.folder_map.get(&ConnectionFolderId(uuid))
+            && let Some(folder) = self.config.folders.get_mut(*idx)
+        {
+            Some(folder)
+        } else {
+            None
+        }
+    }
+
+    fn load_maps(&mut self) {
+        self.connection_map.clear();
+        self.connections_by_folder.clear();
+        self.folder_map.clear();
+        self.folders_by_parent.clear();
+
+        for (idx, connection) in self.config.connections.iter().enumerate() {
+            self.connection_map.insert(connection.id, idx);
+            self.connections_by_folder
+                .entry(connection.folder)
+                .or_default()
+                .push(connection.id);
+        }
+
+        for (idx, folder) in self.config.folders.iter().enumerate() {
+            self.folder_map.insert(folder.id, idx);
+            self.folders_by_parent
+                .entry(folder.parent)
+                .or_default()
+                .push(folder.id);
+        }
+    }
+
+    fn connections_path() -> Result<PathBuf, anyhow::Error> {
+        let Some(dirs) = BaseDirs::new() else {
+            return Err(anyhow!("Failed to retrieve directories"));
+        };
+
+        Ok(dirs
+            .config_dir()
+            .join(shared::APPLICATION_NAME)
+            .join("connections.json"))
+    }
+
+    fn try_load() -> Result<ConnectionConfig, anyhow::Error> {
+        let path = Self::connections_path()?;
+        let reader = BufReader::new(File::open(path)?);
+        Ok(serde_json::from_reader(reader)?)
+    }
+
+    fn try_save(&self) -> Result<(), anyhow::Error> {
+        let path = Self::connections_path()?;
+        let writer = BufWriter::new(File::create(path)?);
+        serde_json::to_writer_pretty(writer, &self.config)?;
+        Ok(())
+    }
+}
+
 #[derive(Debug, Clone, Default, Deserialize, Serialize)]
 pub struct ConnectionConfig {
     connections: Vec<Connection>,
@@ -153,248 +396,5 @@ impl ConnectionFolder {
 impl Default for ConnectionFolder {
     fn default() -> Self {
         Self::new("New folder")
-    }
-}
-
-pub struct ConnectionManager {
-    /// The config containing the connections and folders.
-    config: ConnectionConfig,
-
-    /// Tracks the locations of connections within the full list by [`ConnectionId`].
-    connection_map: HashMap<ConnectionId, usize>,
-
-    /// Tracks the connections within each folder.
-    connections_by_folder: HashMap<Option<ConnectionFolderId>, Vec<ConnectionId>>,
-
-    /// Tracks the locations of folders within the full list by [`ConnectionFolderId`].
-    folder_map: HashMap<ConnectionFolderId, usize>,
-
-    /// Tracks the folders within each parent folder.
-    folders_by_parent: HashMap<Option<ConnectionFolderId>, Vec<ConnectionFolderId>>,
-}
-
-impl Global for ConnectionManager {}
-
-impl ConnectionManager {
-    /// Loads the config from the config file.
-    pub fn load() -> Self {
-        let config = Self::try_load().unwrap_or_else(|_| ConnectionConfig::default());
-        let mut manager = Self {
-            config,
-            connection_map: HashMap::new(),
-            connections_by_folder: HashMap::new(),
-            folder_map: HashMap::new(),
-            folders_by_parent: HashMap::new(),
-        };
-
-        manager.load_maps();
-        manager
-    }
-
-    /// Saves the config to the config file.
-    pub fn save(&mut self) {
-        // TODO: log errors with saving
-        _ = self.try_save();
-    }
-
-    /// Returns a reference to the global [`ConnectionManager`].
-    pub fn global(cx: &App) -> &Self {
-        cx.global::<Self>()
-    }
-
-    /// Returns a mutable reference to the global [`ConnectionManager`].
-    pub fn global_mut(cx: &mut App) -> &mut Self {
-        cx.global_mut::<Self>()
-    }
-
-    /// Returns the connections within the given folder.
-    pub fn connections_for_folder(
-        &self,
-        folder: &Option<ConnectionFolderId>,
-    ) -> Option<&Vec<ConnectionId>> {
-        self.connections_by_folder.get(folder)
-    }
-
-    /// Returns a reference to the connection matching the given [`ConnectionId`].
-    pub fn connection(&self, id: &ConnectionId) -> &Connection {
-        let idx = self.connection_map[id];
-        &self.config.connections[idx]
-    }
-
-    /// Returns a mutable reference to the connection matching the given [`ConnectionId`].
-    pub fn connection_mut(&mut self, id: &ConnectionId) -> &mut Connection {
-        let idx = self.connection_map[id];
-        &mut self.config.connections[idx]
-    }
-
-    /// Returns a reference to the connection matching the given id string, if one is found.
-    pub fn try_connection(&self, id: &SharedString) -> Option<&Connection> {
-        if let Ok(uuid) = Uuid::parse_str(id)
-            && let Some(idx) = self.connection_map.get(&ConnectionId(uuid))
-            && let Some(connection) = self.config.connections.get(*idx)
-        {
-            Some(connection)
-        } else {
-            None
-        }
-    }
-
-    /// Returns a mutable reference to the connection matching the given id string, if one is found.
-    pub fn try_connection_mut(&mut self, id: &SharedString) -> Option<&mut Connection> {
-        if let Ok(uuid) = Uuid::parse_str(id)
-            && let Some(idx) = self.connection_map.get(&ConnectionId(uuid))
-            && let Some(connection) = self.config.connections.get_mut(*idx)
-        {
-            Some(connection)
-        } else {
-            None
-        }
-    }
-
-    /// Adds a new connection to the config.
-    pub fn add_connection(&mut self, connection: Connection) {
-        self.config.connections.push(connection);
-        self.load_maps();
-        self.save();
-    }
-
-    /// Removes the connection with the given [`ConnectionId`] from the config.
-    pub fn remove_connection(&mut self, id: &ConnectionId) {
-        let idx = self.connection_map[id];
-        self.config.connections.swap_remove(idx);
-        self.load_maps();
-        self.save();
-    }
-
-    /// Returns the folders within the given parent folder.
-    pub fn folders_for_parent(
-        &self,
-        parent: &Option<ConnectionFolderId>,
-    ) -> Option<&Vec<ConnectionFolderId>> {
-        self.folders_by_parent.get(parent)
-    }
-
-    /// Returns a reference to the folder matching the given [`ConnectionFolderId`].
-    pub fn folder(&self, id: &ConnectionFolderId) -> &ConnectionFolder {
-        let idx = self.folder_map[id];
-        &self.config.folders[idx]
-    }
-
-    /// Returns a reference to the folder matching the given id string, if one is found.
-    pub fn try_folder(&self, id: &SharedString) -> Option<&ConnectionFolder> {
-        if let Ok(uuid) = Uuid::parse_str(id)
-            && let Some(idx) = self.folder_map.get(&ConnectionFolderId(uuid))
-            && let Some(folder) = self.config.folders.get(*idx)
-        {
-            Some(folder)
-        } else {
-            None
-        }
-    }
-
-    /// Returns a mutable reference to the folder matching the given id string, if one is found.
-    pub fn try_folder_mut(&mut self, id: &SharedString) -> Option<&mut ConnectionFolder> {
-        if let Ok(uuid) = Uuid::parse_str(id)
-            && let Some(idx) = self.folder_map.get(&ConnectionFolderId(uuid))
-            && let Some(folder) = self.config.folders.get_mut(*idx)
-        {
-            Some(folder)
-        } else {
-            None
-        }
-    }
-
-    /// Adds a new folder to the config.
-    pub fn add_folder(&mut self, folder: ConnectionFolder) {
-        self.config.folders.push(folder);
-        self.load_maps();
-        self.save();
-    }
-
-    /// Removes the folder with the given [`ConnectionFolderId`] from the config.
-    ///
-    /// Returns the set of connections that were removed for future processing if needed.
-    pub fn remove_folder(&mut self, id: &ConnectionFolderId) -> HashSet<ConnectionId> {
-        fn remove_inner(
-            manager: &ConnectionManager,
-            id: ConnectionFolderId,
-        ) -> (HashSet<ConnectionId>, HashSet<ConnectionFolderId>) {
-            let mut removed_connections = HashSet::new();
-            let mut removed_folders = HashSet::from([id]);
-
-            if let Some(connections) = manager.connections_for_folder(&Some(id)) {
-                removed_connections.extend(connections);
-            }
-
-            if let Some(folders) = manager.folders_for_parent(&Some(id)) {
-                for id in folders {
-                    let (inner_removed_connections, inner_removed_folders) =
-                        remove_inner(manager, *id);
-                    removed_connections.extend(inner_removed_connections);
-                    removed_folders.extend(inner_removed_folders);
-                }
-            }
-
-            (removed_connections, removed_folders)
-        }
-
-        let (removed_connections, removed_folders) = remove_inner(self, *id);
-        self.config
-            .connections
-            .retain(|connection| !removed_connections.contains(&connection.id));
-        self.config
-            .folders
-            .retain(|folder| !removed_folders.contains(&folder.id));
-
-        self.load_maps();
-        self.save();
-        removed_connections
-    }
-
-    fn load_maps(&mut self) {
-        self.connection_map.clear();
-        self.connections_by_folder.clear();
-        self.folder_map.clear();
-        self.folders_by_parent.clear();
-
-        for (idx, connection) in self.config.connections.iter().enumerate() {
-            self.connection_map.insert(connection.id, idx);
-            self.connections_by_folder
-                .entry(connection.folder)
-                .or_default()
-                .push(connection.id);
-        }
-
-        for (idx, folder) in self.config.folders.iter().enumerate() {
-            self.folder_map.insert(folder.id, idx);
-            self.folders_by_parent
-                .entry(folder.parent)
-                .or_default()
-                .push(folder.id);
-        }
-    }
-
-    fn connections_path() -> Result<PathBuf, anyhow::Error> {
-        let Some(dirs) = BaseDirs::new() else {
-            return Err(anyhow!("Failed to retrieve directories"));
-        };
-
-        Ok(dirs
-            .config_dir()
-            .join(shared::APPLICATION_NAME)
-            .join("connections.json"))
-    }
-
-    fn try_load() -> Result<ConnectionConfig, anyhow::Error> {
-        let path = Self::connections_path()?;
-        let reader = BufReader::new(File::open(path)?);
-        Ok(serde_json::from_reader(reader)?)
-    }
-
-    fn try_save(&self) -> Result<(), anyhow::Error> {
-        let path = Self::connections_path()?;
-        let writer = BufWriter::new(File::create(path)?);
-        serde_json::to_writer_pretty(writer, &self.config)?;
-        Ok(())
     }
 }
